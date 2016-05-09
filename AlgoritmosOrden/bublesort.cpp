6f0ba9ea12c2c7a0309d6a0dd7812719182325b5
senjuana <senju5@hotmail.com>
2016-05-09 13:18:45 -0500

    Bubblesort

---
+++ AlgoritmosOrden/bublesort.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+    void ordenar_numeros(int arreglo[], int lon){
+
+        int Temp;
+
+         for(int i=0;i<lon;i++)
+
+                for(int j=0;j<lon-1;j++)
+
+                    if(arreglo[j]>arreglo[j+1]){// cambia "<" a ">" para cambiar la manera de ordenar
+
+                        Temp=arreglo[j];
+
+                        arreglo[j]=arreglo[j+1];
+
+                        arreglo[j+1]=Temp;}
+
+
+
+                 for(int i=0;i<lon;i++)
+
+                    cout<<arreglo[i]<<endl;
+
+                        }
+
+
+
+
+
+int main(){
+   int lon,n;
+   cout<<"Ingresa el numero de numeros a capturar: ";
+   cin>>lon;
+
+   int arr[lon];
+   for(int i=0;i<lon;i++){
+        cout<<"Ingresa el numero "<<i+1<<": ";
+        cin>>n;
+        arr[i]=n;
+    }
+    cout<<endl<<"Numeros ordenados: "<<endl;
+    ordenar_numeros(arr,lon);
+
+return 0;
+}
