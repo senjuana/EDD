dadcb67e4435c99b2d26a0ee2acc0f1d0482f4f2
senjuana <senju5@hotmail.com>
2016-05-18 12:16:08 -0500

    inclusion y adicion de binarios

--- AlgoritmosOrden/bublesort.cpp
+++ AlgoritmosOrden/bublesort.cpp
@@ -1,27 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
     void ordenar_numeros(int arreglo[], int lon){
-
         int Temp;
-
          for(int i=0;i<lon;i++)
-
                 for(int j=0;j<lon-1;j++)
-
                     if(arreglo[j]>arreglo[j+1]){// cambia "<" a ">" para cambiar la manera de ordenar
-
                         Temp=arreglo[j];
-
                         arreglo[j]=arreglo[j+1];
-
                         arreglo[j+1]=Temp;}
 
-
-
                  for(int i=0;i<lon;i++)
-
                     cout<<arreglo[i]<<endl;
-
                         }
 
 
