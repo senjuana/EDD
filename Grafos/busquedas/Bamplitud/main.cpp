5597d25e403820f35238f7ff264c188b35c2d261
senjuana <jose.ruiz.sc@itszapopan.edu.mx>
2016-12-20 21:44:48 -0600

    Changes to be committed: 	modified:   Makefile 	modified:   main.cpp

--- Grafos/busquedas/Bamplitud/main.cpp
+++ Grafos/busquedas/Bamplitud/main.cpp
@@ -5,12 +5,16 @@
 using namespace std;
 
 int main(){
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
   cola p(20);
 
   p.encolar(3);
   p.encolar(8);
   p.encolar(4);
-  
+  p.encolar(234);
+
   p.print();
   cout<<"\n"; 
   p.desencolar();
