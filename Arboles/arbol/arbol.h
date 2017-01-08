2bdafa9861c4cb0bb8c567b7a9f18c606934fa75
senjuana <jose.ruiz.sc@itszapopan.edu.mx>
2017-01-07 19:34:46 -0600

    cambios menores en el arbol

--- Arboles/arbol/arbol.h
+++ Arboles/arbol/arbol.h
@@ -1,22 +1,14 @@
 #ifndef Arbol
 #define Arbol
-
-typedef struct snode{
-    int data;
-    struct snode *left;
-    struct snode *right;
-}nodo;
-
-typedef struct{
-    nodo *start;
-    nodo *eizq;
-    nodo *eder;
-}Arbol;
-
-void init(Arbol &,int);
-bool vacio(Arbol &);
-void inser(Arbol &lista, int x);
-void print(Arbol &);
-nodo *crearnodo(int);
+template < class T > class arbol {
+	private:
+		typedef struct snode{
+    			int data;
+    			struct snode *left;
+    			struct snode *right;
+		}Nodo;
+		//atributos y metodos auxiliares
+		Nodo root;
+		void insertHelper( Nodo ** ,const 
 
 #endif
