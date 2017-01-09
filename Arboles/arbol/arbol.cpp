4f59d393d7ec3cdb3e8f8b98cb3a7ebcc1b75d2f
senjuana <jose.ruiz.sc@itszapopan.edu.mx>
2017-01-09 15:32:51 -0600

    pequeña modificacion

--- Arboles/arbol/arbol.cpp
+++ Arboles/arbol/arbol.cpp
@@ -1,29 +1,77 @@
-#include "arbol.h"
-#include <bits/stdc++.h>
 using namespace std;
 
+arbol::~arbol() { borrar(root); }
 
-void init(Arbol &a,int){
- a.start=NULL;
- a.eder=NULL;
- a.eizq=NULL;
-}
-bool vacio(Arbol &a){
-return a.start == null;
-}
-
-void inser(Arbol &a, int x){
-    if(vacio(a))a.start->crearnodo(x);
+void arbol::borrar( nodo *root){
+    if(root == NULL){return;}
     else{
-        nodo *p;
-        p=p->a.start;
-        while(p!=NULL && x<)
+        borrar(root->izq);
+        delete root;
+        borrar(root->der);
+    }
+}
+bool arbol::insertar(int x){
+    if(vacio()) {
+        root = new nodo(x,NULL);
+        s++;
+        comp++;
+        return true;
+    }
+    if( llena())return false;
+    nodo *p = root;
+    nodo *q;
+    while(p!=NULL){
+        q = p;
+        comp++;
+        if(x > p->data){
+            p = p->der;
+        }
+        else{
+            p = p->izq;
+        }
 
+    }
+    nodo * aux;
+    aux = new nodo(x,q);
+    if( x > q->data ) {
+        q->der = aux;
+        s++;
+    }
+    else{
+        q->izq = aux;
+        s++;
+    }
+    return true;
+}
+bool arbol::llena(){return s==n;}
+bool arbol::vacio(){return root == NULL;}
+//pruebas1
+void arbol:: inorden(int *arre,int i){ inordeni(root,arre);}
+void arbol::inordeni(nodo * root, int *arre){
+    if(root == NULL){
+        return;
+    }
+    else{
 
-
-
+        inordeni(root->izq,arre);
+        dropdata(arre,root->data);
+        inordeni(root->der,arre);
 
     }
 }
-void print(Arbol &);
-nodo *crearnodo(int);
+void arbol::dropdata(int *arre, int a) { arre[i++]= a; }
+int arbol::getcomp() {
+    return comp;}
+//pruebas
+void arbol::print() {prin(root);}
+void arbol::prin(nodo *root){
+
+    if(root == NULL){
+        return;
+    }
+    else{
+        prin(root->izq);
+        cout<< root->data <<" ";
+        prin(root->der);
+    }
+}
--- Arboles/arbol/arbol.h
+++ Arboles/arbol/arbol.h
@@ -1,14 +1,25 @@
 #ifndef Arbol
 #define Arbol
 template < class T > class arbol {
-	private:
-		typedef struct snode{
+  private:
+	   typedef struct snode{
     			int data;
     			struct snode *left;
     			struct snode *right;
 		}Nodo;
 		//atributos y metodos auxiliares
-		Nodo root;
-		void insertHelper( Nodo ** ,const 
+		Nodo *root;
+    int n;
+    int s;
+
+    void borrar( Nodo *root );
+    void print( Nodo *root );
+
+  public:
+    //metodos escenciales
+    bool insertar( T& x );
+    void inOrden();
+    void postOrden();
+    void preOrden();
 
 #endif
